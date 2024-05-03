bfaf124ba6f7d21ce8e8a775fccd318c5078c676
10RCA <95744399+10RCA@users.noreply.github.com>
2024-05-03 03:33:47 +0300

    Restoranların ekrana verilebilmesi için Postman üzerindeki işlemler halledildi ve API üzerine çalışıldı. Verilerin çekilmesi için indexleri girildi. Restoran ekranında CSS düzenlemeleri yapıldı. Ekran dizaynı (kaydırmalar alt alta gözükme senaryoları vs denendi.)

--- components/ResultsList.js
+++ components/ResultsList.js
@@ -1,13 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import ResultDetail from './ResultDetail';
 
-export default function ResultsList({title, results}) {
-    console.log(results);
+export default function ResultsList({ title, results }) {
+  console.log(results);
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity>
+              <ResultDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+});
