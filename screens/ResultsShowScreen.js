2bf375a1f4e3b3a65624b5a7c714cb015944ba2a
10RCA <95744399+10RCA@users.noreply.github.com>
2024-05-19 00:32:11 +0300

    arama kutucuğuna çeşitli css işlemleri yapıldı ve sonuçlar listelenmesi için endeksler girildi. showscreen kısmında restoran bilgileri ve görüntüleri çekilmesi için işlemler yapıldı. arama ekranında restoranlar görüntülendi.

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -2,6 +2,8 @@ import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ResultsShowScreen({ route }) {
   const [result, setResult] = useState(null);
@@ -22,18 +24,42 @@ export default function ResultsShowScreen({ route }) {
 
   return (
     <View>
-      <Text>{result.name}</Text>
-      <Text>{result.phone}</Text>
+      <Text style={styles.title}>{result.name}</Text>
+      <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <AntDesign name="closecircleo" size={30} color="black" />
+        ) : (
+          <MaterialIcons name="delivery-dining" size={30} color="black" />
+        )}
+      </View>
+
       <FlatList
         data={result.photos}
         renderItem={({ item }) => {
-          return (
-            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
-          );
+          return <Image style={styles.image} source={{ uri: item }} />;
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 180,
+    margin: 10,
+    borderRadius: 20,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 25,
+    marginVertical: 10,
+  },
+  phone: {
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  icon: {
+    alignSelf: 'center',
+  },
+});
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,60 +1,40 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react';
-import SearchBar from   '../components/SearchBar';
-import useResults from '../api/hooks/useResults';
-import { AntDesign } from '@expo/vector-icons';
-import useResults from '../api/hooks/useResults';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
+
 export default function SearchScreen() {
- 
-const [searchApi, results] = useResults()
-console.log(results);
+  const [searchApi, results] = useResults();
+  const [term, setTerm] = useState('');
+  //   console.log(results);
 
-const filterResultsByPrice = (price) =>{
-
-return results.filter((result)=>{
-
-  return result.price === price
-})
-
-}
-
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
   return (
-    <View style={styles.backgroundStyle}>
-       <AntDesign style={styles.iconStyle} name="search1" size={30} color="black" />
-    <TextInput style={styles.inputStyle}
-    
-    placeholder="Ara"
-    autoCorrect={false}
-    autoCapitalize='none'
-    />
-    <SearchBar/>
-    <ResultsList title = 'Öğrenci Dostu Restoranlar'results={filterResultsByPrice('₺')}/>
-    <ResultsList title = 'Uygun Restoranlar'results={filterResultsByPrice('₺₺')}/>
-    <ResultsList title = 'Zengin İşi Restoranlar'results={filterResultsByPrice('₺₺₺')}/>
-
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      <ResultsList
+        title="Ucuz Restoranlar"
+        results={filterResultsByPrice('₺')}
+      />
+      <ResultsList
+        title="Uygun Restoranlar"
+        results={filterResultsByPrice('₺₺')}
+      />
+      <ResultsList
+        title="Pahalı Restoranlar"
+        results={filterResultsByPrice('₺₺₺')}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({
-
-  backgroundStyle:{
-    backgroundColor: 'lightgray',
-    flexDirection: 'row',
-    margin: 10,
-    height: 50,
-    alignItems: 'center',
-    borderRadius: 20,
-
-  },
-  iconStyle:{
-    marginHorizontal: 15,
-
-
-  },
-  inputStyle:{
-    flex: 1,
-    fontSize: 18,
-  },
-})
+const styles = StyleSheet.create({});
