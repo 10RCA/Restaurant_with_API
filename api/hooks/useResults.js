0f3aa09cff350a69a3d837d69471ef30fb6537d5
10RCA <95744399+10RCA@users.noreply.github.com>
2024-06-05 01:12:03 +0300

    hata ekranı ve api bağlantısına uğraşıldı final aşaması yapıldı.

--- api/hooks/useResults.js
+++ api/hooks/useResults.js
@@ -16,7 +16,35 @@ const searchApi = async(searchTerm) => {
 
 }
     useEffect(()=>{
-    searchApi('Toast');
+    searchApi('Toast');import { useEffect, useState } from 'react';
+    import yelp from '../api/yelp';
+    
+    export default () => {
+      const [results, setResults] = useState([]);
+      const [errorMessage, setErrorMessage] = useState('');
+      const searchApi = async (searchTerm) => {
+        try {
+          const response = await yelp.get('/search', {
+            params: {
+              limit: 50,
+              term: searchTerm,
+              location: 'İstanbul',
+            },
+          });
+          setResults(response.data.businesses);
+          setErrorMessage('');
+        } catch (error) {
+          setErrorMessage('Bağlantı Hatası');
+        }
+      };
+    
+      useEffect(() => {
+        searchApi('Toast');
+      }, []);
+    
+      return [searchApi, results, errorMessage];
+    };
+    
 }, []);
     return[searchApi, results];
     //api
